776d04e517d651f1e5b8c92a2551d2e6edba317a
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-05-20 13:07:01 -0600

    Update Inheritance_sub.py

--- Inheritance_sub.py
+++ Inheritance_sub.py
@@ -10,11 +10,15 @@ class Employee:
 #created Manager1 class, it is the highest tier of manager that falls under the Employee class
 class Manager1(Employee):
     security_clearance = 3
+    salary_pay = ''
     department = ''
     floor = ''
+    meal_stipend = ''
 
 #created Front Desk Associate class, floor and department can be assigned as needed.
 class FDAssociate(Employee):
     security_clearance = 0
+    hourly_pay = ''
     department = ''
     floor = ''
+    meal_plan = ''
