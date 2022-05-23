f1ce85497f44bb62b4e7bcda4a68eae69b5fbbd7
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-05-23 11:28:22 -0600

    Delete File_Transfer.py

--- File_Transfer.py
+++
@@ -1,8 +0,0 @@
-import os
-import time
-import shutil
-SRC_PATH = "."
-TARGET_PATH = "../"
-for filename in os.listdir(SRC_PATH):
-    if os.path.getmtime(filename) >= (time.time() - 24*60*60): 
-        shutil.move(os.path.join(SRC_PATH, filename), TARGET_PATH)  
